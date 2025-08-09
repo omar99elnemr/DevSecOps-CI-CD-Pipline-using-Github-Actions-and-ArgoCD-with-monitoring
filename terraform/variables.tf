c5ef59e7b76f8b4ed8e64d377577c7e78210ea00
omar99elnemr <omar99elnemr@gmail.com>
2025-08-09 20:33:12 +0300

    Update eks version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.30"
 }
 
 # EKS Node Group Variables
